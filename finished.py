e9d5a6155fc7c7c7d19bd445b15518c1e31c97d4
BuildTools <unconfigured@null.spigotmc.org>
2020-12-03 23:28:59 +0100

    version1

---
+++ finished.py
@@ -0,0 +1,16 @@
+Python 3.9.0 (default, Oct  7 2020, 23:09:01) 
+[GCC 10.2.0] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============= RESTART: /home/hazel/Downloads/tanuloktantargy(1).py =============
+Tanulók száma(0-1-2): 2
+Tantárgyak száma(0-1-2-3): 3
+Zsebenczy Klopédia
+['Matek', 'Angol', 'Magyar']
+Matek érdemjegy: 2
+Angol érdemjegy: 3
+magyar érdemjegy: 1
+['Balint Dezső', 'Gipsz Jakab', 'Zsebenczy Klopédia'] matematika érdemjegye: 2
+['Balint Dezső', 'Gipsz Jakab', 'Zsebenczy Klopédia'] angol érdemjegye: 3
+['Balint Dezső', 'Gipsz Jakab', 'Zsebenczy Klopédia'] magyar érdemjegye: 1
+>>> 
